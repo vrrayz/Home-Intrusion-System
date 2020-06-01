9e4ad20c80dc9f3ad6507f622c68b276575e5c47
Victor Okonkwo <emmyvic98@gmail.com>
2020-06-01 13:43:30 +0100

    Edit on the User Registration File

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,13 +11,14 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+                        {{-- First Name --}}
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-right">First Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
 
-                                @error('name')
+                                @error('first_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -25,8 +26,41 @@
                             </div>
                         </div>
 
+                        {{-- Last Name --}}
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name</label>
+                        
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name"
+                                    value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                        
+                                @error('last_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- Middle Name --}}
+                        <div class="form-group row">
+                            <label for="middle_name" class="col-md-4 col-form-label text-md-right">Middle Name</label>
+                        
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('middle_name') is-invalid @enderror" name="middle_name"
+                                    value="{{ old('middle_name') }}" required autocomplete="middle_name" autofocus>
+                        
+                                @error('middle_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- Email Address --}}
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -39,6 +73,7 @@
                             </div>
                         </div>
 
+                        {{-- Password --}}
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -53,6 +88,7 @@
                             </div>
                         </div>
 
+                        {{-- Password Confirmation --}}
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
