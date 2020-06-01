3f18e8ce255caf94fe483e3ac03dec549c4a1bc5
Victor Okonkwo <emmyvic98@gmail.com>
2020-06-01 13:47:56 +0100

    Added New Fields to the Users Migration Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,20 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('username')->nullable();
             $table->string('first_name')->nullable();
             $table->string('middle_name')->nullable();
-            $table->string('branch_name')->nullable();
             $table->string('last_name')->nullable();
+            $table->string('branch_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number')->nullable();
             $table->string('date_of_birth')->nullable();
             $table->string('gender')->nullable();
-            $table->string('address')->nullable();
             $table->string('optional_email_address')->nullable();
             $table->string('optional_phone_number')->nullable();
+            $table->string('security_question')->nullable();
+            $table->string('security_answer')->nullable();
             $table->string('user_role');
             $table->rememberToken();
             $table->timestamps();
